7054742ad8a0fc0ab56aeea9072a0742d1e7cae8
Mayank Jangid <mayank.jangid.moon@gmail.com>
2024-10-26 04:39:15 +0530

    preprocess pipeline private

---
+++ model/preprocess_pipeline_private.py
@@ -0,0 +1,35 @@
+import pandas as pd
+import numpy as np
+
+df1 = pd.read_json("../datasets/scrapped/unlabelled/unlabelled1.json")
+df1["username_len"] = df1["username"].apply(len)
+
+def num_count(str):
+    count = 0
+    for i in str:
+        try:
+            x = int(i)
+        except:
+            pass
+        else:
+            count += 1
+    return count
+
+df1["username_num_count"] = df1["username"].apply(num_count)
+
+df1["username_ratio"] = df1["username_num_count"] / df1["username_len"]
+
+df1["biography"] = df1["biography"].replace(np.NaN, "")
+df1["biography_len"] = df1["biography"].apply(len)
+
+df1['isBio'] = df1['biography_len'].apply(lambda x: 1 if x > 0 else 0)
+
+df1 = df1.rename(columns={'followsCount': 'followingCount', 'private': 'isPrivate', 'verified': 'isVerified'})
+df1.columns
+
+columns_use = ["followersCount","followingCount","isPrivate","isVerified","postsCount","username_len","username_num_count","username_ratio","isBio"]
+df1 = df1[columns_use]
+
+df1.dropna(axis="rows", inplace=True)
+
+
